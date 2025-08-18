d2ed26ca40cafc67a2c9a7757b4b684a62b8de2c
RahulSah <sahfamilyinfo@gmail.com>
2025-08-19 01:11:30 +0545

    design rooms page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homes from "./pages/Home";
 import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
 const App = () => {
   const isOwner = useLocation().pathname.includes("owner");
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <div className="min-h-[70vh]">
           <Routes>
             <Route path="/" element={<Homes />} />
+            <Route path="/rooms" element={<AllRooms />} />
           </Routes>
         </div>
         <Footer />
