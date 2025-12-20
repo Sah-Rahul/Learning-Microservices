b5513bb754da3e48c7127c5200313f8ff21d44d2
Sah-Rahul <sahfamilyinfo@gmail.com>
2025-12-20 16:38:22 +0545

    nothing---

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,8 +9,6 @@ const ConnectDB = async () => {
     console.log("Attempting MongoDB connection...");
 
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000, // 5 second timeout
     });
 
